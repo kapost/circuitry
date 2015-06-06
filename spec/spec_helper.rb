eeb390492c7d091d8b73afe106811a8ad17fde00
mhuggins <matt.huggins@gmail.com>
2015-06-05 20:26:44 -0600

    Attempt to fix code coverage reporting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'concord'
 require 'rspec/its'
 require 'pry'
 require 'pry-nav'
-require 'codeclimate-test-reporter'
-
-CodeClimate::TestReporter.start
