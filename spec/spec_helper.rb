ac599f1b8ab64577d3f5ebbb55edddc670d70985
mhuggins <matt.huggins@gmail.com>
2015-06-07 14:29:55 -0600

    Add missing tests, improve error handling, and modify some logic

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,8 @@ require 'concord'
 require 'rspec/its'
 require 'pry'
 require 'pry-nav'
+
+RSpec.configure do |c|
+  c.filter_run_including focus: true
+  c.run_all_when_everything_filtered = true
+end
