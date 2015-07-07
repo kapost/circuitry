b013062a0c39f5907fd5dc13fae337551bb8b31c
mhuggins <matt.huggins@gmail.com>
2015-07-07 15:59:29 -0600

    Add tests for async config options

--- spec/circuitry/configuration_spec.rb
+++ spec/circuitry/configuration_spec.rb
@@ -1,8 +1,47 @@
 require 'spec_helper'
 
 RSpec.describe Circuitry::Configuration, type: :model do
-  describe '#async_strategy=' do
-    pending
+  shared_examples_for 'a validated setting' do |permitted_values, setting_name|
+    def set(value)
+      subject.public_send(:"#{setting}=", value)
+    end
+
+    def get
+      subject.public_send(setting)
+    end
+
+    let(:setting) { setting_name }
+
+    permitted_values.each do |value|
+      describe "with valid value #{value}" do
+        it 'does not raise an error' do
+          expect { set(value) }.to_not raise_error
+        end
+
+        it 'changes the config value' do
+          set(value)
+          expect(get).to eq value
+        end
+      end
+    end
+
+    describe 'with invalid value' do
+      it 'raises an error' do
+        expect { set(:fake) }.to raise_error ArgumentError
+      end
+
+      it 'does not change the config value' do
+        expect { set(:fake) rescue nil }.to_not change { get }
+      end
+    end
+  end
+
+  describe '#publish_async_strategy=' do
+    it_behaves_like 'a validated setting', Circuitry::Publisher.async_strategies, :publish_async_strategy
+  end
+
+  describe '#subscribe_async_strategy=' do
+    it_behaves_like 'a validated setting', Circuitry::Subscriber.async_strategies, :subscribe_async_strategy
   end
 
   describe '#aws_options' do
